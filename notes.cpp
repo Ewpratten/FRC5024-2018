3ee6285e84088f878c52dc9a903d5e3df0d916f6
Evan Pratten <evan@localhost.localdomain>
2018-01-15 18:31:06 -0500

    rename

--- notes.cpp
+++ notes.cpp
@@ -1,12 +1,11 @@
-new drive program
-file>>new project>> C++ example>>arcade drive>> (give it a new name)
-
-src > robot.cpp
+//new drive program
+//file>>new project>> C++ example>>arcade drive>> (give it a new name)
+//src > robot.cpp
 // change " to < or > in include
 // dont touch libs
 // include <ctre/Phoenix.h>
 // make a robot.h file
-#include <"robot.h"
+#include "robot.h"
 // move all code except start_robot_class(robot) to robot.h
 // move teleop periotic to robot.cpp at top
 // in robot.h remove everything in curly brackets of teleop periotic; replace cbrackets with ";"
